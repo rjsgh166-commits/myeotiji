5c5abc5b618f0aee92e4c44235f6788023be233a
gungun <323842901+rjsgh166-commits@users.noreply.github.com>
2026-09-03 11:27:47 +0900

    Fix web manifest icon purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -21,7 +21,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
